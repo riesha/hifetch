ea57b0ec8ba27d38a0b93eea24a7269345e9ed37
seleneia <shwistar@gmail.com>
2020-10-18 12:51:41 +0200

    begin

--- main.go
+++ main.go
@@ -2,28 +2,29 @@ package main
 
 import (
     "fmt"
-    "os"
-    "text/tabwriter"
+	"os"
 )
 
 func main() {
+
 var c1 ="\033[35m" 
 var rs ="\033[0m"
+
 var envars = map[string]string{}
 
 	hostname, err := os.Hostname()
 	if err != nil {
 		fmt.Println(err)
 	}
-	
-	envars["usr"] = os.Getenv("USER")
-	envars["hos"] = hostname
-	envars["shl"] = os.Getenv("SHELL")
 
+envars["usr"] = os.Getenv("USER")
+envars["hos"] = hostname
+envars["shl"] = os.Getenv("SHELL")
 
-
-	w := tabwriter.NewWriter(os.Stdout, 10, 1, 1, ' ', 0)
-	fmt.Fprintln(w,c1+"usr:" + rs + "  " + envars["usr"]+c1+"@"+rs+ envars["hos"]+"\t")
-	fmt.Fprintln(w,c1+"shl:" + rs + "  " + envars["shl"])
-	w.Flush()
+info := `
+	%susr:%s	%s@%s
+	%sshl:%s	%s
+	`
+t := fmt.Sprintf(info,c1,rs,envars["usr"],envars["hos"],c1,rs,envars["shl"])
+fmt.Println(t)
 }
